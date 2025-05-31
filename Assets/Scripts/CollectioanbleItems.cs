15e057116a50ddc6fd3e2283cc3a0a18c25d13c8
JeanPierr-Bs <jeanpierrvalenciaestrada@gmail.com>
2025-05-31 15:43:46 -0500

    Update Shoot and Gui

--- Assets/Scripts/CollectioanbleItems.cs
+++ Assets/Scripts/CollectioanbleItems.cs
@@ -5,6 +5,7 @@ public class CollectioanbleItems : MonoBehaviour
 {
     public GameObject handTarget;
     private GameObject pickedObject = null;
+    public Texture2D puntero;
 
     private void Update()
     {
@@ -13,7 +14,7 @@ public class CollectioanbleItems : MonoBehaviour
             // Mantener la posición del objeto en la mano
             pickedObject.transform.position = handTarget.transform.position;
 
-            if (Input.GetKey(KeyCode.R))
+            if (Input.GetKey("r"))
             {
                 // Soltar el objeto
                 pickedObject.transform.SetParent(null, true);
@@ -26,16 +27,23 @@ public class CollectioanbleItems : MonoBehaviour
                     physics.velocity = Vector2.zero;
                 }
 
+                var shootScript = pickedObject.GetComponentInParent<Shoot>();
+                if (shootScript != null)
+                {
+                    Debug.Log("Arma soltada");
+                    shootScript.isHeld = false;
+                }
+
                 pickedObject = null;
             }
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    public void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Collectionable") && pickedObject == null)
         {
-            if (Input.GetKey(KeyCode.E))
+            if (Input.GetKey("e"))
             {
                 pickedObject = other.gameObject;
 
@@ -44,11 +52,28 @@ public class CollectioanbleItems : MonoBehaviour
                 {
                     physics.simulatePhysics = false;
                 }
-
+                
                 pickedObject.transform.SetParent(handTarget.transform, false);
                 pickedObject.transform.localRotation = Quaternion.Euler(0, 90, 0);
                 pickedObject.transform.position = handTarget.transform.position;
+
+                // <<<<<<<<<<<<<< Aquí usamos GetComponentInParent
+                var shootScript = pickedObject.GetComponentInParent<Shoot>();
+                if (shootScript != null)
+                {
+                    Debug.Log("¡Arma recogida correctamente!");
+                    shootScript.isHeld = true;
+                }
+                else
+                {
+                    Debug.LogWarning("No se encontró el script 'Shoot' en el objeto recogido");
+                }
             }
         }
+    }
+    void OnGUI()
+    {
+        Rect rect = new Rect(Screen.width / 2, Screen.height / 2, puntero.width / 2, puntero.height / 2);
+        GUI.DrawTexture(rect, puntero);
     }
 }
